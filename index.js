d6fcabb1c10af4a7398c36baff7f9bbc99aed347
Agia <agia@pm.me>
2023-02-24 15:43:18 +0000

    Tiny refactoring and comment edits to make everything a little clearer / cleaner

--- index.js
+++ index.js
@@ -3,20 +3,19 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+// Import function to render HTML
+const render = require("./src/page-template.js");
 
 // Node packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-// Other packages
+// ** VARIABLES ** //
+// Variables to hold path to output file
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
-
-
-// ** VARIABLES ** //
 // Array to hold team member objects
 const team = [];
 
@@ -289,7 +288,7 @@ async function addIntern() {
     optionsPrompt(answers);
 }
 
-// Generates index.html with user data
+// Generates team.html with user data
 function generateHTML(data) {
     fs.writeFile(outputPath, render(data), function(error) {
         if (error) {
